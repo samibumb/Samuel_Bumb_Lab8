dea77f8195ed09576937ed9f14b53b5565b2b058
Sami <bmb.sami@yahoo.com>
2021-11-06 13:15:21 +0200

    Add project files.

---
+++ Data/Samuel_Bumb_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Samuel_Bumb_Lab8.Models;
+
+namespace Samuel_Bumb_Lab8.Data
+{
+    public class Samuel_Bumb_Lab8Context : DbContext
+    {
+        public Samuel_Bumb_Lab8Context (DbContextOptions<Samuel_Bumb_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Samuel_Bumb_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Samuel_Bumb_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Samuel_Bumb_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Books/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Samuel_Bumb_Lab8.Data;
+using Samuel_Bumb_Lab8.Models;
+
+namespace Samuel_Bumb_Lab8.Pages.Books
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Samuel_Bumb_Lab8.Data.Samuel_Bumb_Lab8Context _context;
+
+        public DetailsModel(Samuel_Bumb_Lab8.Data.Samuel_Bumb_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public Book Book { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Book = await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Book == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
